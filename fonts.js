52afb1a436081bdb68cf04c82dfed37f9b8f79c5
nabiha-Z <nabihazubair100@gmail.com>
2023-03-25 11:01:26 +0500

    screens refractoring

--- fonts.js
+++
@@ -1,8 +0,0 @@
-import * as Font from 'expo-font';
-
-export async function loadFonts() {
-  await Font.loadAsync({
-    'Roboto-Regular': require('../fonts/Roboto-Regular.ttf'),
-    'Roboto-Bold': require('../fonts/Roboto-Bold.ttf'),
-  });
-}
